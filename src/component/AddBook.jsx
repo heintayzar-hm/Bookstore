57ef5bd4eecf9959cbf062b2869b0bcd9611907e
Hein Tay Zar <heintayzarhm@gmail.com>
2022-12-13 02:06:28 +0630

    Add add and remove func

--- src/component/AddBook.jsx
+++ src/component/AddBook.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class AddBook extends React.Component {
   constructor(props) {
     super();
     this.props = props;
+    this.state = {
+      author: '',
+      title: '',
+    };
+  }
+
+  changeHandler = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    const { addBook } = this.props;
+    const { title, author } = this.state;
+    if (title.trim() && author.trim()) {
+      addBook(title, author);
+      this.setState({
+        author: '',
+        title: '',
+      });
+    }
   }
 
   render() {
+    // eslint-disable-next-line no-unused-vars
+    const { author, title } = this.state;
     return (
       <>
         <article data-testid="form-container">
           <h2>Add New Book</h2>
-          <form>
-            <input name="title" data-testid="book-title-input" type="text" placeholder="Book Title" required />
-            <input name="author" data-testid="author-title-input" type="text" placeholder="Author" required />
+          <form onSubmit={this.submitHandler}>
+            <input name="title" onChange={this.changeHandler} data-testid="book-title-input" type="text" placeholder="Book Title" value={title} required />
+            <input name="author" onChange={this.changeHandler} data-testid="author-title-input" type="text" placeholder="Author" value={author} required />
             <button type="submit">Add Book</button>
           </form>
         </article>
@@ -23,4 +51,7 @@ class AddBook extends React.Component {
   }
 }
 
+AddBook.propTypes = {
+  addBook: PropTypes.func.isRequired,
+};
 export default AddBook;
--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { clickCheckStatus } from '../redux/categories/categoriesReducer';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Category extends React.Component {
   constructor(props) {
     super();
     this.props = props;
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+
+  buttonHandler() {
+    const { dispatch } = this.props;
+    dispatch(clickCheckStatus());
   }
 
   render() {
+    const { categories } = this.props;
     return (
       <>
+        { categories ? `${categories}` : null}
         <div>
-          <button data-testid="check-status" type="button">Check Status</button>
+          <button data-testid="check-status" onClick={this.buttonHandler} type="button">Check Status</button>
         </div>
       </>
     );
   }
 }
-export default Category;
+
+const mapStateToProps = (state) => ({
+  // specify which state data to provide to the component
+  categories: state.categories,
+});
+
+Category.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  categories: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.any),
+    PropTypes.string,
+  ]).isRequired,
+};
+export default connect(mapStateToProps)(Category);
